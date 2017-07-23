eafd200390bdf7a4b5eb32953face31a8f0a70da
Damon Foster <damon.fstr@gmail.com>
2017-07-23 12:03:40 +0100

    adding logging for identifier

--- src/controllers/search_controller.js
+++ src/controllers/search_controller.js
@@ -5,36 +5,43 @@ var identifier;
 setupSearchController = function (controller) {
     controller.hears(['user (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
         identifier = message.match[1];
+        console.log("Making search request with identifier: " + identifier);
         makeSearchRequest(controller, bot, message, identifier, 'userid');
     });
 
     controller.hears(['user <mailto:([^\?]*)[|]([^\?]*)>'], 'direct_message,direct_mention,mention', function (bot, message){
         identifier = message.match[1];
+        console.log("Making search request with identifier: " + identifier);
         makeSearchRequest(controller, bot, message, identifier, 'useremail');
     });
 
     controller.hears(['lead <mailto:([^\?]*)[|]([^\?]*)>'], 'direct_message,direct_mention,mention', function (bot, message){
         identifier = message.match[1];
+        console.log("Making search request with identifier: " + identifier);
         makeSearchRequest(controller, bot, message, identifier, 'leademail');
     });
 
     controller.hears(['user ([A-z][A-Za-z]*\s+[A-Za-z]*)|([A-z][A-Za-z]*)'], 'direct_message,direct_mention,mention', function (bot, message){
         identifier = message.match[1];
+        console.log("Making search request with identifier: " + identifier);
         makeSearchRequest(controller, bot, message, identifier, 'username');
     });
 
     controller.hears(['lead ([A-z][A-Za-z]*\s+[A-Za-z]*)|([A-z][A-Za-z]*)'], 'direct_message,direction_mention,mention', function (bot, message){
         identifier = message.match[1];
+        console.log("Making search request with identifier: " + identifier);
         makeSearchRequest(controller, bot, message, identifier, 'leadname');
     });
 
     controller.hears(['user ((\+))(\d|\s|\+|\.|\(|\)|\-*)*'], 'direct_message,direct_mention,mention', function (bot, message){
         identifier = message.match[1];
+        console.log("Making search request with identifier: " + identifier);
         makeSearchRequest(controller, bot, message, identifier, 'userphone');
     });
 
     controller.hears(['lead ((\+))(\d|\s|\+|\.|\(|\)|\-*)*'], 'direct_message,direct_mention,mention', function (bot, message){
         identifier = message.match[1];
+        console.log("Making search request with identifier: " + identifier);
         makeSearchRequest(controller, bot, message, identifier, 'leadphone')
     });
 };
